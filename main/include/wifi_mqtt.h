00db2ee275fe7f0576d92bdede9f7513ad300805
Bunyamin Korkmaz <bnykrkmz@gmail.com>
2026-01-27 17:20:00 +0300

    Final Version

--- main/include/wifi_mqtt.h
+++ main/include/wifi_mqtt.h
@@ -2,26 +2,23 @@
 #define WIFI_MQTT_H
 
 #include <stdbool.h>
+#include "secrets.h" 
 
-// --- Wi-Fi Settings ---
-#define WIFI_SSID       "Bunyamin"
-#define WIFI_PASS       "bnyo02bnyo"
+// --- Wi-Fi settings---
+#define WIFI_SSID       SECRET_WIFI_SSID
+#define WIFI_PASS       SECRET_WIFI_PASS
 
-// --- Adafruit IO Settings ---
-#define MQTT_BROKER_URL "mqtt://io.adafruit.com"
-#define MQTT_USERNAME   "BnyKorkmaz"
-#define MQTT_KEY        "aio_iNgR89jJUrzmfzBZ3MzaPxroEKkf"
+#define MQTT_BROKER_URL SECRET_MQTT_URL
+#define MQTT_USERNAME   SECRET_MQTT_USER
+#define MQTT_KEY        SECRET_MQTT_KEY
 
-// --- Feed Paths (Topics) ---
-// Note: Must be in the format "Username/feeds/feed_name" (Case sensitive!)
-#define TOPIC_POWER       "BnyKorkmaz/feeds/power"
-#define TOPIC_CURRENT     "BnyKorkmaz/feeds/current"
-#define TOPIC_COST        "BnyKorkmaz/feeds/cost"
+#define TOPIC_POWER     SECRET_TOPIC_POWER
+#define TOPIC_CURRENT   SECRET_TOPIC_CURRENT
+#define TOPIC_COST      SECRET_TOPIC_COST
 
-// Function Declarations
 void wifi_mqtt_init(void);
 void mqtt_send_data(const char* topic, float value);
 
-extern bool mqtt_connected; // Flag for external access to connection status
+extern bool mqtt_connected; 
 
 #endif
